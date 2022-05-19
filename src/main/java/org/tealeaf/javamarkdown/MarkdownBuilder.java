706ed2d135e21943f778ee187c203a3020afe1c9
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-05-19 11:29:26 -0400

    Implement Markup Code

--- src/main/java/org/tealeaf/javamarkdown/MarkdownBuilder.java
+++ src/main/java/org/tealeaf/javamarkdown/MarkdownBuilder.java
@@ -1,39 +1,56 @@
 package org.tealeaf.javamarkdown;
 
+import org.tealeaf.javamarkdown.markup.Bold;
+import org.tealeaf.javamarkdown.markup.Code;
+import org.tealeaf.javamarkdown.markup.Italic;
+import org.tealeaf.javamarkdown.markup.Strikethrough;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
 public class MarkdownBuilder {
 
-    private boolean addSpaceBeforeInserting = true;
-
+    private final StringWriter stringWriter;
 
     public MarkdownBuilder() {
-
+        this.stringWriter = new StringWriter();
     }
 
-    public MarkdownBuilder add(Object object) {
+    public MarkdownBuilder(StringWriter stringWriter) {
+        this.stringWriter = stringWriter;
+    }
+
+    public MarkdownBuilder append(Object object) {
+        stringWriter.append(object.toString());
         return this;
     }
 
-    public MarkdownBuilder addBold(Object object) {
+    public MarkdownBuilder appendBold(Object object) throws IOException {
+        new Bold(object).toWriter(stringWriter);
         return this;
     }
 
-    public MarkdownBuilder addItalic(Object object) {
+    public MarkdownBuilder appendItalic(Object object) throws IOException {
+        new Italic(object).toWriter(stringWriter);
         return this;
     }
 
-    public MarkdownBuilder addStrikethrough(Object object) {
+    public MarkdownBuilder appendStrikethrough(Object object) throws IOException {
+        new Italic(object).toWriter(stringWriter);
         return this;
     }
 
-    public MarkdownBuilder addLink(String url, Object object) {
+    public MarkdownBuilder appendCode(Object object) throws IOException {
+        new Code(object).toWriter(stringWriter);
         return this;
     }
 
-    public MarkdownBuilder addImage(String source) {
-        return this;
+    public StringWriter getStringWriter() {
+        return stringWriter;
     }
 
-    public void setAddSpaceBeforeInserting(boolean addSpaceBeforeInserting) {
-        this.addSpaceBeforeInserting = addSpaceBeforeInserting;
+    @Override
+    public String toString() {
+        return stringWriter.toString();
     }
 }
