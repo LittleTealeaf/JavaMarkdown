75f980d9d968112db90b613d238b3f0b98271b04
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-05-19 09:28:14 -0400

    Theoretically, this should make releases

---
+++ .github/python/release.py
@@ -0,0 +1,19 @@
+# This uses the latest github tag pulled in order to dictate the next publish release version
+
+import os
+
+currentVersion = os.popen('git describe --tags --abbrev=0').read().split('\n')[0]
+
+# Increment the last value
+parts = currentVersion.split('.')
+parts[-1] = str(int(parts[-1]) + 1)
+version = '.'.join(parts)
+
+# Check the commit list
+commits = os.popen(f'git log {currentVersion}...HEAD --oneline').read().split('\n')
+print(len(commits) - 1)
+
+
+print("Releasing Version",version)
+
+os.system(f'gh release create {version} --generate-notes')
