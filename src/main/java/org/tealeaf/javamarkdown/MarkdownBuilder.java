a9ef3951306e1d34146726d120905f906393786f
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-05-20 15:35:33 -0400

    Added Javadocs to MarkdownElement

--- src/main/java/org/tealeaf/javamarkdown/MarkdownBuilder.java
+++ src/main/java/org/tealeaf/javamarkdown/MarkdownBuilder.java
@@ -46,7 +46,7 @@ public class MarkdownBuilder extends Writer {
 
     public MarkdownBuilder append(Object object) throws IOException {
         if(object instanceof MarkdownElement) {
-            return appendMarkdownItem((MarkdownElement) object);
+            return appendMarkdownElement((MarkdownElement) object);
         } else {
             append(object.toString());
             return this;
@@ -58,39 +58,39 @@ public class MarkdownBuilder extends Writer {
     }
 
     public MarkdownBuilder appendBold(Object object) throws IOException {
-        return appendMarkdownItem(new Bold(object));
+        return appendMarkdownElement(new Bold(object));
     }
 
     public MarkdownBuilder appendItalic(Object object) throws IOException {
-        return appendMarkdownItem(new Italic(object));
+        return appendMarkdownElement(new Italic(object));
     }
 
     public MarkdownBuilder appendStrikethrough(Object object) throws IOException {
-        return appendMarkdownItem(new Strikethrough(object));
+        return appendMarkdownElement(new Strikethrough(object));
     }
 
     public MarkdownBuilder appendCode(Object object) throws IOException {
-        return appendMarkdownItem(new Code(object));
+        return appendMarkdownElement(new Code(object));
     }
 
     public MarkdownBuilder appendLink(Object content, String href) throws IOException {
-        return appendMarkdownItem(new Link(content,href));
+        return appendMarkdownElement(new Link(content, href));
     }
 
     public MarkdownBuilder appendImage(String url) throws IOException {
-        return appendMarkdownItem(new Image(url));
+        return appendMarkdownElement(new Image(url));
     }
 
     public MarkdownBuilder appendBulletList(Object... items) throws IOException {
-        return appendMarkdownItem(new BulletList(items));
+        return appendMarkdownElement(new BulletList(items));
     }
 
     public MarkdownBuilder appendNumberedList(Object... items) throws IOException {
-        return appendMarkdownItem(new NumberedList(items));
+        return appendMarkdownElement(new NumberedList(items));
     }
 
 
-    protected MarkdownBuilder appendMarkdownItem(MarkdownElement markdownElement) throws IOException {
+    protected MarkdownBuilder appendMarkdownElement(MarkdownElement markdownElement) throws IOException {
         if(markdownElement.requiresNewlineBefore() && lastChar != '\n') {
             write("\n");
         }
--- src/main/java/org/tealeaf/javamarkdown/MarkdownElement.java
+++ src/main/java/org/tealeaf/javamarkdown/MarkdownElement.java
@@ -54,20 +54,56 @@ public abstract class MarkdownElement {
      *     - item 1
      *     - item 2
      * </pre>
+     * <p>This method is natively used in {@link MarkdownBuilder#appendMarkdownElement(MarkdownElement)} to handle adding newlines when necessary</p>
      *
-     * @see MarkdownBuilder#appendMarkdownItem(MarkdownElement)
      * @return {@code true} if element requires a new line beforehand, {@code false} otherwise
+     *
+     * @see MarkdownBuilder#appendMarkdownElement(MarkdownElement)
      */
     public abstract boolean requiresNewlineBefore();
 
     /**
-     * Whether to insert a {@code \n} after the formatted element
+     * <p>Whether a new line should be added after inserting the element into a document. This is particularly useful for structures that need to have a newline after it
+     * gets rendered</p>
+     * <p>For example, if some text was printed immediately after a list, it would render as:</p>
+     * <pre>
+     *     - item 1
+     *     - item 2
+     *     - item 3some text
+     * </pre>
+     * <p>Thus, by requiring a new line after the element is rendered, we get the following output instead:</p>
+     * <pre>
+     *     - item 1
+     *     - item 2
+     *     - item 3
+     *     some text
+     * </pre>
+     * <p>This method is natively used in {@link MarkdownBuilder#appendMarkdownElement(MarkdownElement)} to handle adding newlines when necessary</p>
      *
-     * @return {@code true} if there needs to be at least one {@code \n} after the formatted element, {@code false} if not.
+     * @return {@code true} if element requires a new line after, {@code false} otherwise
+     *
+     * @see MarkdownBuilder#MarkdownBuilder()
      */
     public abstract boolean requiresNewlineAfter();
+
+    /**
+     * <p>Checks the type of some passed object. This method is specific to the element and throws an {@link IllegalContentsException} if an illegal object type is passed
+     * through. This prevents instances such as tables or other similar elements being directly inserted into elements such as a {@link org.tealeaf.javamarkdown.markup.Bold
+     * Bold} or {@link org.tealeaf.javamarkdown.lists.NumberedList NumberedList} element.
+     * </p>
+     *
+     * @param object Object to check the data type of
+     *
+     * @throws IllegalContentsException if the object passed is an illegal type for the element
+     */
     protected abstract void checkType(Object object) throws IllegalContentsException;
 
+    /**
+     * <p>Compares an object to a set of illegal MarkdownElement classes. If the object is an instance of any element, it will throw an {@link IllegalContentsException}</p>
+     * @deprecated See {@link #checkType(Object)} for new method of checking
+     * @param object
+     * @param classStream
+     */
     @Deprecated
     protected void testIllegalTypes(Object object, Set<Class<? extends MarkdownElement>> classStream) {
         Optional<Class<? extends MarkdownElement>> illegalType = classStream.parallelStream().filter(item -> item.isInstance(object)).findFirst();
