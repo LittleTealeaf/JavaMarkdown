72c35a9fdbc10847b0f8bf90ea7b7c4e50a0fe4d
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-05-20 15:47:51 -0400

    Added javadocs: IllegalContentsException

--- src/main/java/org/tealeaf/javamarkdown/IllegalContentsException.java
+++ src/main/java/org/tealeaf/javamarkdown/IllegalContentsException.java
@@ -1,19 +1,30 @@
 package org.tealeaf.javamarkdown;
 
 /**
+ * <p>
+ * When thrown, indicates that an illegal element was attempted to be passed as content of another element. For example, if a
+ * {@link org.tealeaf.javamarkdown.lists.NumberedList NumberedList} was passed into a {@link org.tealeaf.javamarkdown.markup.Italic Italic} element, then this error woudl
+ * be thrown
+ * </p>
+ *
  * @author Thomas Kwashnak
  * @since 0.0.8
  */
 public class IllegalContentsException extends RuntimeException {
 
-    private final Class<? extends MarkdownElement> invalidClass;
+    private final Class<?> illegalClass;
 
-    public IllegalContentsException(Class<? extends MarkdownElement> invalidClass) {
-        this.invalidClass = invalidClass;
+    /**
+     * <p>Creates a new IllegalContents exception with the illegal class in question</p>
+     *
+     * @param illegalClass The illegal class passed as contents
+     */
+    public IllegalContentsException(Class<?> illegalClass) {
+        this.illegalClass = illegalClass;
     }
 
     @Override
     public String getMessage() {
-        return String.format("Illegal Class class passed into object: %s", invalidClass.toString());
+        return String.format("Illegal Class class passed into object: %s", illegalClass.toString());
     }
 }
