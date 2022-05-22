fd9b95ba936bb903b90ed476e03b478c45de0f39
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-05-22 12:12:57 -0400

    Added Markup Javadoc

--- src/main/java/org/tealeaf/javamarkdown/types/Markup.java
+++ src/main/java/org/tealeaf/javamarkdown/types/Markup.java
@@ -5,26 +5,53 @@ import org.tealeaf.javamarkdown.MarkdownElement;
 import java.io.IOException;
 import java.io.Writer;
 
-public abstract class Markup extends InlineElement {
+/**
+ * Represents markdown elements that indicates a simple markup syntax to the content text, such as {@code **words**} to indicate <b>bold</b> text.
+ * @author Thomas Kwashnak
+ * @since 0.0.1
+ */
+public class Markup extends InlineElement {
 
+    /**
+     * The syntax used in the markup language
+     */
     protected final String syntax;
+    /**
+     * Object to insert into the markup
+     */
     protected final Object object;
 
+    /**
+     * Creates a new markup element with a content object and the wrapping syntax
+     * @param object Content object to display
+     * @param syntax String syntax to put on either side of the content
+     */
     public Markup(Object object, String syntax) {
         this.object = object;
         this.syntax = syntax;
         checkType(object);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@code false}
+     */
     @Override
     public boolean requiresNewlineBefore() {
         return false;
     }
 
+    /**
+     * {@inheritDoc}
+     * @return {@code false}
+     * @since 0.0.7
+     */
     @Override
     public boolean requiresNewlineAfter() {
         return false;
     }
+
 
     @Override
     public Writer toWriter(Writer writer) throws IOException {
