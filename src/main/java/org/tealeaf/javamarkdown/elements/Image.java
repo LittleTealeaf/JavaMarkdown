eada4abae65e79cd42d5122d39d4c81abc4b4a8a
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-06-04 13:43:47 -0400

    Added simple image constructor

--- src/main/java/org/tealeaf/javamarkdown/elements/Image.java
+++ src/main/java/org/tealeaf/javamarkdown/elements/Image.java
@@ -2,6 +2,14 @@ package org.tealeaf.javamarkdown.elements;
 
 public class Image extends Link {
 
+    /**
+     * @since 0.0.12
+     * @param src Image Source
+     */
+    public Image(String src) {
+        super("",src);
+    }
+
     public Image(String content, String src) {
         super(content, src);
     }
