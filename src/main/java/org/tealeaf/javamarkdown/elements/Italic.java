cda6d7a2198e03d65ccd750e289ae28f35f0daab
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-07-12 13:17:11 -0400

    Javadocs: `Italic` Fixes #34

--- src/main/java/org/tealeaf/javamarkdown/elements/Italic.java
+++ src/main/java/org/tealeaf/javamarkdown/elements/Italic.java
@@ -2,9 +2,19 @@ package org.tealeaf.javamarkdown.elements;
 
 import org.tealeaf.javamarkdown.types.Markup;
 
+/**
+ * <p>Renders some content in <i>Italics</i></p>
+ * @author Thomas Kwashnak
+ * @since 0.0.10
+ */
 public class Italic extends Markup {
 
-    public Italic(Object object) {
-        super(object, "*");
+    /**
+     * <p>Creates an {@code Italics} object, and sets the content to be rendered in <i>italics</i></p>
+     * @since 0.0.10
+     * @param content Object content to render
+     */
+    public Italic(Object content) {
+        super(content, "*");
     }
 }
